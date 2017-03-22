aeae5aa9048eaf0b477bfc176572c42321c1b46f
Mohammed Islam <mohammed.islam@tallwave.com>
2017-03-21 21:45:26 -0700

    Add some styling

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,47 +1,45 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 import Foundation
 import PlaygroundSupport
 
-//-----------------------------------------------------------------------------
-//____________________________ Thread ____________________________________
-// https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/Multithreading/CreatingThreads/CreatingThreads.html
-
-//let thread = Thread { 
+/*: 
+ ## Thread
+ [Creating Threads](https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/Multithreading/CreatingThreads/CreatingThreads.html)
+*/
+//let thread = Thread {
 //    print("monkey")
 //}
 //thread.start()
 
-//-----------------------------------------------------------------------------
-//____________________________ Run Loop ____________________________________
-// https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/Multithreading/RunLoopManagement/RunLoopManagement.html
-
+/*:
+ ## Run Loop
+ [Run Loop Management](https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/Multithreading/RunLoopManagement/RunLoopManagement.html)
+ */
 //let timer = Timer(timeInterval: 1.0, repeats: true) { timer in
 //    print("poop")
 //}
 //let runner = RunLoop.current
 //runner.add(timer, forMode: RunLoopMode.defaultRunLoopMode)
 
-//-----------------------------------------------------------------------------
-//____________________________ Queues ______________________________________________
-// https://developer.apple.com/library/content/documentation/General/Conceptual/ConcurrencyProgrammingGuide/OperationQueues/OperationQueues.html
-
-// ---- Example of using a blocking queue
+/*:
+ ## Queues
+ [Dispatch Queues](https://developer.apple.com/library/content/documentation/General/Conceptual/ConcurrencyProgrammingGuide/OperationQueues/OperationQueues.html)
+ */
+//: ### Example of using a blocking queue
 //print("moop")
 //DispatchQueue.global().sync {
 //    blockingPrint(message: "bloop", afterSeconds: 1)
 //}
 //print("woop")
 
-// ---- Example of using a non-blocking/asynchronous queue
+//: ### Using a non-blocking/asynchronous queue
 //print("moop")
 //DispatchQueue.global().async {
 //    blockingPrint(message: "bloop", afterSeconds: 1)
 //}
 //print("woop")
 
-// ---- Example of using concurrent queue asynchronously/non-blocking
+//: ### Using concurrent queue asynchronously/non-blocking
 //DispatchQueue.global().async {
 //    blockingPrint(message: "poo1", randomlyAfterSeconds: 1)
 //}
@@ -54,7 +52,7 @@ import PlaygroundSupport
 //    blockingPrint(message: "poo3", randomlyAfterSeconds: 1)
 //}
 
-// ---- Example of using consecutive/serial main Queue asynchronously
+//: ### Using consecutive/serial main Queue asynchronously
 //DispatchQueue.main.async {
 //    blockingPrint(message: "poo1", randomlyAfterSeconds: 1)
 //}
